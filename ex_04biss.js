03eec64050da07157fdccc2ccde4fedb60a503cb
Abdallah94212 <abdallah.borji@epitech.digital>
2024-10-09 19:46:29 +0200

    Create ex_04biss.js

---
+++ ex_04biss.js
@@ -0,0 +1,14 @@
+function houdini(name) {
+$("#" + name).hide();  
+$("." + name).hide();  
+}
+
+$(document).ready(function() {
+$("#hide-test").click(function() {
+houdini("test");  
+});
+$("#hide-unique").click(function() {
+
+houdini("unique");  
+});
+});
